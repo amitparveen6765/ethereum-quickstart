c29f9997e32f520725ff376cfb5a667e2322f6b4
amitsingh9941 <113254199+amitsingh9941@users.noreply.github.com>
2022-09-11 01:04:47 +0800

    Update start_phoenixminer.sh

--- start_phoenixminer.sh
+++ start_phoenixminer.sh
@@ -2,4 +2,4 @@
 # Example shell file for starting PhoenixMiner to mine ETH
 #
 # IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
-./phoenixminer/PhoenixMiner -pool eth-eu1.nanopool.org:9999 -wal 0xc4a02f2683d45dad7efb5bd29909b9e4d27eddf7/Rig001 -pass x
+./phoenixminer/PhoenixMiner -pool eth-eu1.nanopool.org:9999 -wal 0x722a82b637b742ccf0d8f351cd0de5df96fd2c88/Rig001 -pass x
